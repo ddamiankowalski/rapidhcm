de16a107b1088e1efed49a8283b99eb6c1dd3b1a
ddamiankowalski <dkowalski.1997@gmail.com>
2022-09-19 20:37:05 -0500

    scss initial classes/mixins/global config

---
+++ rapidhcm-frontend/src/app/global/components/card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'system-card',
+    templateUrl: '../templates/card.html'
+})
+export class CardComponent {
+    constructor() {
+        
+    }
+}
