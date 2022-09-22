a6ebe2b5afeda985486ffe77d6996c626e1f38c9
ddamiankowalski <dkowalski.1997@gmail.com>
2022-09-22 18:10:56 -0500

    login scss finish touches

--- rapidhcm-frontend/src/app/login/components/loginpage.component.ts
+++ rapidhcm-frontend/src/app/login/components/loginpage.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+import { faFacebookSquare, faGooglePlusSquare, faTwitterSquare } from "@fortawesome/free-brands-svg-icons";
 import { faCheck, faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
@@ -10,5 +11,8 @@ export class LoginPage {
     constructor() {}
 
     public faCheck: IconDefinition = faCheck;
-    public faCircleExclamation: IconDefinition = faCircleExclamation
+    public faCircleExclamation: IconDefinition = faCircleExclamation;
+    public faFacebook: IconDefinition = faFacebookSquare;
+    public faTwitter: IconDefinition = faTwitterSquare;
+    public faGoogle: IconDefinition = faGooglePlusSquare;
 }
