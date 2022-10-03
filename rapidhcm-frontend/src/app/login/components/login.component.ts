6c155ec0fb846a19d420a8cad17c6566e2b0a2d3
ddamiankowalski <dkowalski.1997@gmail.com>
2022-10-03 22:07:48 +0200

    changes in validation

--- rapidhcm-frontend/src/app/login/components/login.component.ts
+++ rapidhcm-frontend/src/app/login/components/login.component.ts
@@ -2,7 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Component } from "@angular/core";
 import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 import { faFacebookSquare, faGooglePlusSquare, faTwitterSquare } from "@fortawesome/free-brands-svg-icons";
-import { faEye } from "@fortawesome/free-regular-svg-icons";
+import { faEye, faEyeSlash } from "@fortawesome/free-regular-svg-icons";
 import { faCheck, faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
@@ -18,8 +18,15 @@ export class LoginComponent {
     public faTwitter: IconDefinition = faTwitterSquare;
     public faGoogle: IconDefinition = faGooglePlusSquare;
     public faEye: IconDefinition = faEye;
+    public faEyeSlash: IconDefinition = faEyeSlash;
 
-    public login: string = '';
+    public togglePasswordVisibility() {
+        this.showPassword = !this.showPassword;
+    }
+
+    public showPassword: boolean = false;
+
+    public username: string = '';
     public password: string = '';
     public isLoading: boolean = false;
 
@@ -29,6 +36,18 @@ export class LoginComponent {
 
     public loginChange(event: any) {
         console.log(event)
+    }
+
+    public submitLogin(event: any) {
+        console.log('eslo', event)
+    }
+
+    public generateErrMessage(username: any, password: any) {
+        if(username.control.errors && Object.keys(username.control.errors).length && username.control.touched) {
+            return username.control.errors;
+        } else if(password.control.errors && Object.keys(password.control.errors).length && password.control.touched) {
+            return password.control.errors;
+        }
     }
 
     public handleClick(event: any) {
